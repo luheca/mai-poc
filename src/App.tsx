7a28dedb0335c607db3ab936e444479cefc2c785
Luis Herrera <hc.luis@gmail.com>
2026-03-14 20:16:26 -0500

    Initial.

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+export default function App() {
+  return (
+    <Box display="flex" justifyContent="center" alignItems="center" minHeight="100vh">
+      <Typography variant="h3" component="h1">mai-poc</Typography>
+    </Box>
+  );
+}
